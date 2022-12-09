a57d646747ca95139d92b92466a53bb1898d7dc1
Batuhan Yumak <batuhanyumak@Macbook-Batuhan.local>
2022-12-09 12:27:29 +0100

    navbar_aangepas

--- myworld/members/urls.py
+++ myworld/members/urls.py
@@ -5,8 +5,14 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('hotels/', views.hotel, name='hotel'),
     path('amsterdam/', views.amsterdam, name='ams'),
-    # path('hotels/<int:hotel_id>', views.hotel_detail, name='hotel_detail'),
+    path('antwerpen/', views.antwerpen, name='antw'),
+    path('athene/', views.athene, name='ath'),
+    path('bangkok/', views.bangkok, name='bak'),
+    path('barcelona/', views.barcelona, name='bar'),
+    path('berlijn/', views.berlijn, name='ber')
+
 ]
+
 
 # urlpatterns(r'^$', 'index', name='index'),
 # urlpatterns(r'^blog$', 'horel', name='blog'),
