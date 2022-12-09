c51369a375e0c841568fc89a3d23d74d1feb53ff
BatuhanYumak <b.yumak70@gmail.com>
2022-12-09 13:36:22 +0100

    detail_b

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -14,3 +14,34 @@ def amsterdam(request):
      'amsterdam': Informatie.objects.filter(city_name = 'Amsterdam')
 }
     return render(request, 'amsterdam.html', context)
+  
+def antwerpen(request):
+    context = {
+     'antwerpen': Informatie.objects.filter(city_name = 'Antwerpen')
+}
+    return render(request, 'antwerpen.html', context)
+  
+    
+def athene(request):
+    context = {
+     'athene': Informatie.objects.filter(city_name = 'Athene')
+}
+    return render(request, 'athene.html', context)
+  
+def bangkok(request):
+    context = {
+     'bangkok': Informatie.objects.filter(city_name = 'Bangkok')
+}
+    return render(request, 'bangkok.html', context)
+  
+def barcelona(request):
+    context = {
+     'barcelona': Informatie.objects.filter(city_name = 'Barcelona')
+}
+    return render(request, 'barcelona.html', context)    
+
+def berlijn(request):
+    context = {
+     'berlijn': Informatie.objects.filter(city_name = 'Berlijn')
+}
+    return render(request, 'berlijn.html', context)  
