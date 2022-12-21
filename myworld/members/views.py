c9234ffdc4a70291b1e38a9ebc58f0f894b0aa04
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2022-12-21 11:42:02 +0100

    regsitratie pagina toegevoegd

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -73,10 +73,25 @@ def login_view(request):
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 
-def register_view(request):
+def signup_view(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('home')
+ 
+ 
+ 
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'signup.html', {'error': 'Invalid login credentials'})
+    else:
+        return render(request, 'signup.html')
