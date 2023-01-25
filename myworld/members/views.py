a9bbac6b26e21e8c9044edf8747572fd19bbdeb1
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2023-01-25 13:45:06 +0100

    probeer gebruiker op te slaan in de db

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import *
+from .models import RegistrationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth import authenticate
+
 def index(request):
   template = loader.get_template('index.html')
   return HttpResponse(template.render())
@@ -54,44 +59,29 @@ def berlijn(request):
     return render(request, 'berlijn.html', context)
 
 
-from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
+#login en registratie pagina
 
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
-        password = request.POST['password']
+        password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('home')
         else:
-            return render(request, 'login.html', {'error': 'Invalid login credentials'})
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
     else:
         return render(request, 'login.html')
-
-from django.contrib.auth import login
-from django.shortcuts import render, redirect
-
+ 
 def signup_view(request):
     if request.method == 'POST':
-        form = RegistrationForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('home')
- 
- 
- 
-def signup_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            return render(request, 'signup.html', {'error': 'Invalid login credentials'})
     else:
-        return render(request, 'signup.html')
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+                  
--- myworld/myworld/urls.py
+++ myworld/myworld/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', include('members.urls')),
-    path('admin/', admin.site.urls),    
+    path('admin/', admin.site.urls),  
+
 ]
 
 
