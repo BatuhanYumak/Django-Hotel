78ce31bbef7e4ef4ae116084ac395ea57570389d
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2023-02-06 13:23:03 +0100

    registratie pagina aanpassen met widgets

--- myworld/members/forms.py
+++ myworld/members/forms.py
@@ -1,2 +1,12 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'password': forms.TextInput(attrs={'class': 'form-control'})
+        }
